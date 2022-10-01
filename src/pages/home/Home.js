25354184a4ba7c6f24efc3e15828663a0e5ec425
srinaththota <srinath.thota@gmail.com>
2022-10-01 21:49:12 +0200

    handled home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,13 +2,6 @@ import { Navigate } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import { useSelector,useDispatch } from "react-redux";
 import { FetchProductsAction } from '../../store/actions/products/FetchProductsAction'
-const arrayIsEqual = (a1, a2) => 
-  a1 === a2 ||
-  a1.length === a2.length &&
-  a1.every((f,i) => 
-    f.id === a2[i].id &&
-    f.name === a2[i].name
-  )
 
 const Home = ()=>{
   const dispatch=useDispatch();
